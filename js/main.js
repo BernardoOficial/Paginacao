6697af3cba2ff8ff0a4e1bbfba2bb52f8f35c232
Bernardo Pereira Oliveira <bernardo.pereira@foster.com.br>
2021-03-31 23:33:05 -0300

    Finalizando a paginação

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const $ = document.querySelector.bind(document);
+
 const htmlCard = $("[data-cards]");
 
-const paginacao = {
+const btnPaginaAtual = $("[data-pagina-atual]");
+const btnPrimeiraPagina = $("[data-primeira-pagina]");
+const btnUltimaPagina = $("[data-ultima-pagina]");
+const btnVoltarPagina = $("[data-anterior-pagina]");
+const btnProximaPagina = $("[data-proxima-pagina]");
+const btn2PaginasSeguintes = $("[data-duas-pagina-seguintes]");
+const btn2PaginasAnteriores = $("[data-duas-pagina-anteriores]");
+
+const state = {
   paginaAtual: 1,
   itensPorPagina: 5,
   totalDeItens: 100,
@@ -9,89 +18,124 @@ const paginacao = {
   itens: Array(100),
 
   calcularTotalDePaginas() {
-    this.totalDePaginas = this.totalDeItens / this.itensPorPagina;
-  },
-    
-  buttons() {
-    const btnPaginaAtual = $("[data-pagina-atual]");
-    const btnPrimeiraPagina = $("[data-primeira-pagina]");
-    const btnUltimaPagina = $("[data-ultima-pagina]");
+    state.totalDePaginas = state.totalDeItens / state.itensPorPagina;
+  }
+};
 
-    const btn2PaginasAnterior = $("[data-duas-pagina-anterior]");
-    const btn2PaginasProxima = $("[data-duas-pagina-proximo]");
-
-    btnPrimeiraPagina.addEventListener('click', () => {
-        this.paginaAtual = 1;
-        btnPaginaAtual.textContent = this.paginaAtual;
-        this.renderizar();
-    });
-    btnUltimaPagina.addEventListener('click', () => {
-        this.paginaAtual = this.totalDePaginas;
-        btnPaginaAtual.textContent = this.paginaAtual;
-        this.renderizar();
-    });
-
-    const btnPaginaAnterior = $("[data-anterior-pagina]");
-    const btnPaginaProxima = $("[data-proxima-pagina]");
-
-    btnPaginaAnterior.addEventListener('click', () => {
-        if(this.paginaAtual <= 1) {
-            return;
-        }
-        this.paginaAtual = this.paginaAtual - 1;
-        btnPaginaAtual.textContent = this.paginaAtual;
-        this.renderizar();
-    });
-    btnPaginaProxima.addEventListener('click', () => {
-        if (this.paginaAtual >= this.totalDePaginas) {
-          return;
-        }
-        this.paginaAtual = this.paginaAtual + 1;
-        btnPaginaAtual.textContent = this.paginaAtual;
-
-        if (this.paginaAtual >= 3) {
-            btn2PaginasAnterior.style.display = "block"
-            btn2PaginasAnterior.textContent = this.paginaAtual - 2;
-            btn2PaginasAnterior.addEventListener("click", () => {
-            });
-        } else {
-            btn2PaginasAnterior.style.display = "none";
-        }
-
-        btn2PaginasProxima.addEventListener("click", () => {});
-
-
-        this.renderizar();
-    });
-  },
-
-  renderizar() {
-    const primeiroItem = Math.floor((this.paginaAtual - 1) * 5);
-    const ultimoItem = primeiroItem + this.itensPorPagina;
-
+const lista = {
+  atualizar() {
+    const primeiroItem = Math.floor((state.paginaAtual - 1) * 5);
+    const ultimoItem = primeiroItem + state.itensPorPagina;
+  
     htmlCard.innerHTML = "";
     for (let i = primeiroItem; i < ultimoItem; i++) {
-      htmlCard.innerHTML += this.itens[i];
+      htmlCard.innerHTML += state.itens[i];
     }
+  }
+}
+
+const controls = {
+  init() {
+    state.paginaAtual <= 2
+      ? (btn2PaginasAnteriores.style.display = "none")
+      : (btn2PaginasAnteriores.textContent = state.paginaAtual - 2);
+    state.paginaAtual >= 18
+      ? (btn2PaginasSeguintes.style.display = "none")
+      : (btn2PaginasSeguintes.textContent = state.paginaAtual + 2);
+
+    btnPaginaAtual.textContent = state.paginaAtual;
   },
-
-  gerarTemplate() {
-    const gerarTemplateComNumeros = (array) => {
-      for (let i = 0; i < array.length; i++) {
-        array[i] = `<article>${i + 1}</article>`;
-      }
-      return array;
-    };
-
-    this.itens = gerarTemplateComNumeros(this.itens);
+  ultimaPagina() {
+    state.paginaAtual = state.totalDePaginas;
+    uptate();
   },
-
-  inicializacao() {
-    this.gerarTemplate();
-    this.renderizar();
-    this.calcularTotalDePaginas();
-    this.buttons();
+  primeiraPagina() {
+    state.paginaAtual = 1;
+    uptate();
+  },
+  proximaPagina() {
+    if (state.paginaAtual >= 20) {
+      return;
+    }
+    state.paginaAtual = ++state.paginaAtual;
+    uptate();
+  },
+  voltarPagina() {
+    if (state.paginaAtual <= 1) {
+      return;
+    }
+    state.paginaAtual = --state.paginaAtual;
+    uptate();
+  },
+  duasPaginasSeguintes() {
+    if (state.paginaAtual >= 19) {
+      return;
+    }
+    state.paginaAtual = state.paginaAtual + 2;
+    uptate();
+  },
+  duasPaginasAnteriores() {
+    if (state.paginaAtual <= 2) {
+      return;
+    }
+    btn2PaginasAnteriores.textContent = state.paginaAtual;
+    state.paginaAtual = state.paginaAtual - 2;
+    uptate();
   },
 };
 
-paginacao.inicializacao();
+const stateButtons = {
+  atualizar() {
+    btn2PaginasAnteriores.textContent = state.paginaAtual - 2;
+    btnPaginaAtual.textContent = state.paginaAtual;
+    btn2PaginasSeguintes.textContent = state.paginaAtual + 2;
+
+    stateButtons.podeExibirEmTela();
+  },
+  podeExibirEmTela() {
+    state.paginaAtual <= 2
+      ? (btn2PaginasAnteriores.style.display = "none")
+      : (btn2PaginasAnteriores.style.display = "block", btn2PaginasAnteriores.textContent = state.paginaAtual - 2);
+    state.paginaAtual >= 19
+      ? (btn2PaginasSeguintes.style.display = "none")
+      : (btn2PaginasSeguintes.style.display = "block", btn2PaginasSeguintes.textContent = state.paginaAtual + 2);
+  }
+};
+
+const handleClick = () => {
+
+  const htmlControls = [
+    [btnPrimeiraPagina, controls.primeiraPagina],
+    [btnUltimaPagina, controls.ultimaPagina],
+    [btnProximaPagina, controls.proximaPagina],
+    [btnVoltarPagina, controls.voltarPagina],
+    [btn2PaginasSeguintes, controls.duasPaginasSeguintes],
+    [btn2PaginasAnteriores, controls.duasPaginasAnteriores]
+  ]
+
+  htmlControls.forEach(([element, controle]) =>
+    element.addEventListener("click", controle)
+  );
+}
+
+function uptate() {
+  lista.atualizar();
+  stateButtons.atualizar();
+}
+
+function gerarTemplate(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = `<article>${i + 1}</article>`;
+  }
+  return array;
+};
+
+function inicializacao() {
+    state.itens = gerarTemplate(state.itens);
+    lista.atualizar();
+    state.calcularTotalDePaginas();
+    controls.init();
+    handleClick();
+}
+
+inicializacao();
